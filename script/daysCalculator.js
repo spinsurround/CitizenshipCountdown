1957ab265a8931068b4c32075e320600fc655248
Tengbo Zou <tengbozou@Tengbos-MacBook-Pro.local>
2021-08-13 13:22:57 -0700

    refactored format

--- script/daysCalculator.js
+++ script/daysCalculator.js
@@ -1,6 +1,6 @@
 let applicationDate = new Date(2025, 03, 22);
-let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24));
+let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24)) - 90;
 
-diffInDays = Math.max(0, diffInDays - 90);
+diffInDays = Math.max(0, diffInDays);
 
 document.getElementById("countdown").textContent = `${diffInDays} Days`;
