bd56a633150c4f85cf6ca2b1122ccdadb51d56c6
Tengbo Zou <tengbozou@Tengbos-MacBook-Pro.local>
2021-08-13 12:30:50 -0700

    fixed a bug

--- script/daysCalculator.js
+++ script/daysCalculator.js
@@ -1,6 +1,6 @@
 let applicationDate = new Date(2025, 03, 22);
 let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24));
 
-diffInDays = Math.max(0, diffInDays);
+diffInDays = Math.max(0, diffInDays - 90);
 
-document.getElementById("countdown").textContent = `${diffInDays - 90} Days`;
+document.getElementById("countdown").textContent = `${diffInDays} Days`;
