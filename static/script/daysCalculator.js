814a09eec2a18db0bdc1bf5e718588cd942b7855
Tengbo Zou <tengbozou@Tengbos-MacBook-Pro.local>
2021-08-13 08:31:41 -0700

    added icon

--- static/script/daysCalculator.js
+++
@@ -1,6 +0,0 @@
-let applicationDate = new Date(2025, 03, 22);
-let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24));
-
-diffInDays = Math.max(0, diffInDays);
-
-document.getElementById("countdown").textContent = `${diffInDays} Days`;
