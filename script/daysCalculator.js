10bbaad8bf376577895f5119fe2b29c502237848
Tengbo Zou <tzou33@gatech.edu>
2022-04-12 22:55:44 -0700

    Fix 90 days minus bug

--- script/daysCalculator.js
+++ script/daysCalculator.js
@@ -1,9 +1,9 @@
-let applicationDate = new Date(2025, 03, 22);
-let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24)) - 90;
+let applicationDate = new Date(new Date(2025, 03, 22).getTime() - 90*1000*60*60*24);
+let diffInDays = Math.floor((applicationDate - Date.now())/(1000*60*60*24));
 
 diffInDays = Math.max(0, diffInDays);
 
 years = Math.floor(diffInDays/365)
 days = diffInDays%365
 
-document.getElementById("countdown").textContent = `${years} years ${days} days until 03-22-2025`;
+document.getElementById("countdown").textContent = `${years} years ${days} days until ${applicationDate.toLocaleDateString()}`;
